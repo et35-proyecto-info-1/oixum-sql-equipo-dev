b5d38f040a524a32c269e85d7c1687b8804b42a6
EstradaErik <alexanderestrada100@gmail.com>
2019-10-21 17:02:43 -0300

    TC

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/query', function(req, res) {
-  connection.query('SELECT * FROM estudiantes', function (error, results, fields) {
+  connection.query('SELECT * FROM test.estudiantes', function (error, results, fields) {
     if (error) throw error;
     res.json(results);
   });
