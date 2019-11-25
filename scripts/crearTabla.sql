5340eb6b962d170cd997208985aee84c7f2eaa36
EstradaErik <alexanderestrada100@gmail.com>
2019-11-25 16:03:38 -0300

    AgregaV5

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -13,6 +13,26 @@ INSERT INTO test.estudiantes VALUES
 ('','Emiliano','Ravioles','2002-09-26'),
 ('','Matias','Ñoquis','2002-09-16'),
 ('','Alexander','Hamburgesa','2001-06-01'),
-('','Juan','Asado','1999-02-25');
+('','Juan','Asado','1999-02-25'),
+('','Leandro','Pancho','1980-09-09'),
+('','German','Milanesa','2001-09-03');
 
+insert into persona values
+(476,"11222333"),
+(477,"20123145"),
+(478,"11222333"),
+(480,"36478298"),
+(481,"94857365"),
+(482,"08007220"),
+(483,"12345678");
 
+CREATE TABLE Persona(
+CodP int,
+DNI varchar(12),
+constraint PK_Per primary key(CodP)
+);
+
+insert into test.persona values
+(476,"11222333"),
+(477,"20123145"),
+(478,"11222333");
