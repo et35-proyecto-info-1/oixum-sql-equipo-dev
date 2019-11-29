9be5cdfefee36ef68374e76f93204cc1089b46b3
EstradaErik <alexanderestrada100@gmail.com>
2019-11-29 09:27:44 -0300

    CambiosV6

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -1,38 +1,53 @@
+DROP DATABASE IF EXISTS test;
 CREATE DATABASE IF NOT EXISTS test;
 
 CREATE TABLE IF NOT EXISTS test.estudiantes (
-	id INT NOT NULL AUTO_INCREMENT,
-	nombre varchar(100) NOT NULL,
-	comidaPreferida varchar(100) NULL,
+	id INT NOT NULL,
+	nombre varchar(20) NOT NULL,
+	comidaPreferida varchar(40) NULL,
 	fechaNacimiento DATE NOT NULL,
 	CONSTRAINT estudiantes_PK PRIMARY KEY (id)
 );
 
 INSERT INTO test.estudiantes VALUES
-('','Erik','Fideos','2001-07-27'),
-('','Emiliano','Ravioles','2002-09-26'),
-('','Matias','Ñoquis','2002-09-16'),
-('','Alexander','Hamburgesa','2001-06-01'),
-('','Juan','Asado','1999-02-25'),
-('','Leandro','Pancho','1980-09-09'),
-('','German','Milanesa','2001-09-03');
+(1,'Erik','Fideos','2001/07/27'),
+(2,'Emiliano','Ravioles','2002/09/26'),
+(3,'Matias','Arroz','2002-09-16'),
+(4,'Alexander','Hamburgesa','2001/06/01'),
+(5,'Juan','Asado','1999/02/25'),
+(6,'Leandro','Pizza','1980/09/09'),
+(7,'German','Pollo','2001/09/03');
 
-insert into persona values
-(476,"11222333"),
-(477,"20123145"),
-(478,"11222333"),
-(480,"36478298"),
-(481,"94857365"),
-(482,"08007220"),
-(483,"12345678");
 
-CREATE TABLE Persona(
-CodP int,
-DNI varchar(12),
-constraint PK_Per primary key(CodP)
+CREATE TABLE IF NOT EXISTS test.persona(
+DNI int not null,
+Nombre varchar(20),
+constraint PK_Per primary key(DNI)
 );
 
 insert into test.persona values
-(476,"11222333"),
-(477,"20123145"),
-(478,"11222333");
+(11222333,"Miguel"),
+(20123145,"Marcelo"),
+(88238213,"Margarita"),
+(36478298,"Luis"),
+(94857365,"Melian"),
+(08007220,"Juan"),
+(12345678,"Pedro"),
+(44263814,"Matias"),
+(19785452,"Micaela"),
+(43570272,"Dylan"),
+(44520290,"Sofia"),
+(44230903,"Federico"),
+(25410014,"Claudia");
+
+
+-- CREATE TABLE pers(
+-- DNI int,
+-- Nombre varchar(20),
+-- constraint PK_Per primary key(DNI)
+-- );
+
+-- insert into pers values
+-- (11222333,"Miguel"),
+-- (20123145,"Marcelo"),
+-- (88238213,"Margarita");
--- src/app.js
+++ src/app.js
@@ -1,41 +1,29 @@
 var express = require('express');
 var connection = require('./connection');
-var exphbs  = require('express-handlebars');
 var path = require('path');
 
 var app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.render('home')
-});
-
-app.get('/query', function(req, res) {
-  connection.query('SELECT * FROM test.estudiantes', 
-  function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
-  });
+  res.render('index');
 });
 
 app.post('/', function(req, res){
-  const query = req.body.consulta;
+  const query = req.body.query;
   connection.query(query, function(error, results, fields){
-    res.render('home',{
-      resultados: results,
-      columnas: fields,
+    res.render('index',{
+      results: results,
+      fields: fields,
       error: error,
       query: query
     });
   });
 });
-
-
-
 
 app.listen(3000, function() {
   connection.connect();
